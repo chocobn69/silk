fc076de820b94b8bd132eabf40ee224638cc589e
Nicolas Baccelli <nicolas.baccelli@abc-culture.fr>
2017-03-01 08:48:19 +0100

    Add the possibility to specify test execution

--- docker/tests.sh
+++ docker/tests.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
-TEST='example_app'
+if [ $# -eq 0 ]; then
+    TEST='example_app'
+else
+    TEST=$@
+fi
 docker-compose -f docker/docker-compose.yml run --rm web /code/project/manage.py test --keepdb $TEST
